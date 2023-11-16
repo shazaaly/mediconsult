88fb4e2f7df5852e4adda28269745d722e8aa392
shazaaly <shaza.aly@gmail.com>
2023-11-16 13:53:16 +0200

    restructure

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,15 @@
 from app import app
 
-@app.route("/")
-@app.route("/home")
-def home():
-    return "hello"
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Shaza'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+<h1>Hello, ''' + user.get('username') + '''!</h1>
+</body>
+</html>'''
---
+++ mediconsult.py
@@ -0,0 +1,7 @@
+
+"""
+This module contains the main application logic for app.
+"""
+
+
+from app import app

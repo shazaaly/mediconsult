b4783fb61b03b8132f25fa2832c0a453fbf53243
shazaaly <shaza.aly@gmail.com>
2023-11-16 12:45:00 +0200

    init

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,5 @@
-from flask import Flask,redirect,url_for,render_template,request
+from flask import Flask
 
-app=Flask(__name__)
-@app.route('/',methods=['GET','POST'])
-def home():
-	if request.method=='POST':
-		# Handle POST Request here
-		return render_template('index.html')
-	return render_template('index.html')
+app = Flask(__name__)
 
-if __name__ == '__main__':
-	#DEBUG is SET to TRUE. CHANGE FOR PROD
-	app.run(port=5000,debug=True)
+from app import routes

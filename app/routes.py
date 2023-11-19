7f1bf3e96ecf9cad55e3cf400bd67ec27afc3fa2
shazaaly <shaza.aly@gmail.com>
2023-11-20 01:56:35 +0200

    register

--- app/routes.py
+++ app/routes.py
@@ -42,7 +42,7 @@ def login():
 
     if form.validate_on_submit():
         user=User.query.filter_by(username=form.username.data).first()
-        if user is None and not user.check_password(form.password.data):
+        if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
@@ -59,7 +59,7 @@ def logout():
     return redirect(url_for('index'))
 
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     """create a new user
     """
@@ -67,7 +67,7 @@ def register():
         return redirect('index')
     form = RegisterForm()
     if form.validate_on_submit():
-        user=User(username=form.username.data, emeil=form.email.data)
+        user=User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()

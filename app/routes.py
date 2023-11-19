56a7478c962496ddd6a3b571070351ae91153f90
shazaaly <shaza.aly@gmail.com>
2023-11-19 13:07:10 +0200

    generating links

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template,flash, redirect
+from flask import render_template,flash, redirect, url_for
 from forms import LoginForm
 from app import app
 
@@ -27,7 +27,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash(" Logged in successfully user {}, remeber me : {}".format(form.username.data, form.remember_me.data))
-        return redirect("/index")
+        return redirect(url_for("index"))
     return render_template('login.html', form=form, title="Login")
 
 from app import routes

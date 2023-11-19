29142c00f6a537c7c965e5bc9d7d07a838952160
shazaaly <shaza.aly@gmail.com>
2023-11-19 12:31:56 +0200

    loginForm

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,10 @@ It creates a Flask object and sets up any necessary configuration options.
 """
 
 from flask import Flask
+from config import Config
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,34 @@
+from flask import render_template,flash, redirect
+from forms import LoginForm
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
     user = {'username': 'Shaza'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-<h1>Hello, ''' + user.get('username') + '''!</h1>
-</body>
-</html>'''
+    return render_template('index.html', username=user['username'], title="MediConsult", posts=posts)
+
+
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(" Logged in successfully user {}, remeber me : {}".format(form.username.data, form.remember_me.data))
+        return redirect("/index")
+    return render_template('login.html', form=form, title="Login")
+
+from app import routes
+

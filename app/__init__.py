32e78905a7554e2a98e71ddc32dc8e90a6469741
shazaaly <shaza.aly@gmail.com>
2023-11-19 13:42:18 +0200

    Flask-SQLAlchemy and Flask-Migrate initialization

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,14 @@ It creates a Flask object and sets up any necessary configuration options.
 
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import routes
+db = SQLAlchemy(app)
+migrate  = Migrate(app, db)
+
+from app import routes, models

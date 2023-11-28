3b0f1e20ee47b3dd1904fe6f9678cbc48d37cd8b
shazaaly <shaza.aly@gmail.com>
2023-11-28 20:09:04 +0200

    flask-bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -13,21 +13,20 @@ from flask_login import LoginManager
 from flask_mail import Mail
 import logging
 from logging.handlers import SMTPHandler
+from flask_bootstrap import Bootstrap
 
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__)
 app.config.from_object(Config)
 mail = Mail(app)
-
-
 db = SQLAlchemy(app)
 migrate  = Migrate(app, db)
-
 login = LoginManager(app)
 login.login_view = 'login'
+Bootstrap(app)
 
-if not app.debug:
-    if app.config['MAIL_SERVER']:
+#if not app.debug:
+""" if app.config['MAIL_SERVER']:
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
             auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
@@ -37,10 +36,10 @@ if not app.debug:
         mail_handler = SMTPHandler(
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
             fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            toaddrs=app.config['ADMIN'], subject='Microblog Failure',
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
 
-
+"""
 from app import routes, models, errors
